bd4d0fc419058a51c6fada5ee74c072e61ce4490
GioGio4 <g.ceccobelli@outlook.it>
2023-02-27 15:46:08 +0100

    Create components + import in App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script>
 
+import Appheader from './components/AppHeader.vue';
+import contents from './components/AppContents.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+
+  components: {
+    Appheader,
+    contents,
+    AppFooter
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <Appheader />
+  <contents />
+  <AppFooter />
+</template>
 
 <style scoped></style>
