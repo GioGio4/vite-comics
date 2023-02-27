01706c374223830dff9474203de8a6ddcde18b17
GioGio4 <g.ceccobelli@outlook.it>
2023-02-27 17:25:19 +0100

    Add data in AppFooter components

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,146 @@
 <script>
+export default {
+    data() {
+        return {
+            // Footer link Data
+            ComicsLink: {
+                title: "DC COMICS",
+                linkList: [
+                    {
+                        title: "Characters",
+                        link: "#"
+                    },
+                    {
+                        title: "Comics",
+                        link: "#"
+                    },
+                    {
+                        title: "Movies",
+                        link: "#"
+                    },
+                    {
+                        title: "Tv",
+                        link: "#"
+                    },
+                    {
+                        title: "Games",
+                        link: "#"
+                    },
+                    {
+                        title: "Collectibles",
+                        link: "#"
+                    },
+                    {
+                        title: "Video",
+                        link: "#"
+                    },
+                    {
+                        title: "Fans",
+                        link: "#"
+                    },
+                    {
+                        title: "News",
+                        link: "#"
+                    },
+                    {
+                        title: "Shop",
+                        link: "#"
+                    },
+                ]
+            },
 
+            ShopLink: {
+                title: "SHOP",
+                linkList: [
+                    {
+                        title: "Shop DC",
+                        link: "#",
+                    },
+                    {
+                        title: "Shop DC Collectibles",
+                        link: "#",
+                    }
+                ]
+            },
+
+            DcLink: {
+                title: "DC",
+                linkList: [
+                    {
+                        title: "Terms of Use",
+                        link: "#"
+                    },
+                    {
+                        title: "Privacy Policy",
+                        link: "#"
+                    },
+                    {
+                        title: "Ad Choice",
+                        link: "#"
+                    },
+                    {
+                        title: "Advertising",
+                        link: "#"
+                    },
+                    {
+                        title: "Jobs",
+                        link: "#"
+                    },
+                    {
+                        title: "Subscription",
+                        link: "#"
+                    },
+                    {
+                        title: "Talent Workshop",
+                        link: "#"
+                    },
+                    {
+                        title: "CPSC Certificates",
+                        link: "#"
+                    },
+                    {
+                        title: "Rating",
+                        link: "#"
+                    },
+                    {
+                        title: "Shop Help",
+                        link: "#"
+                    },
+                    {
+                        title: "Contact Us",
+                        link: "#"
+                    },
+                ]
+            },
+
+            SitesLink: {
+                title: "SITES",
+                linkList: [
+                    {
+                        title: "DC",
+                        link: "#"
+                    },
+                    {
+                        title: "Mad Magazine",
+                        link: "#"
+                    },
+                    {
+                        title: "DC Kids",
+                        link: "#"
+                    },
+                    {
+                        title: "DC Universe",
+                        link: "#"
+                    },
+                    {
+                        title: "DC Power Visa",
+                        link: "#"
+                    },
+                ]
+            }
+        }
+    }
+}
 </script>
 
 <template>
