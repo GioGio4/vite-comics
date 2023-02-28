d970927693ede964df7f3bfd36e66fdbe2810388
GioGio4 <g.ceccobelli@outlook.it>
2023-02-28 16:53:14 +0100

    Add card components + props data (comic)

--- src/components/AppContents.vue
+++ src/components/AppContents.vue
@@ -1,20 +1,41 @@
 <script>
+import Card from './Card.vue';
+
+export default {
+
+    props: {
+        ComicsList: Array,
+    },
+
+    components: { Card },
+};
 
 </script>
 
 <template>
     <section>
-        <h1>--> Content goes Here &lt;-- </h1>
+
+        <div class="card-container">
+            <Card v-for="comic in ComicsList" :ComicInfo="comic" />
+
+        </div>
     </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 /* Debug */
+
 section {
-    height: 100px;
-    background-color: black;
+    background-color: #1c1c1c;
     color: white;
     padding: 10px 200px;
-    ;
+
+    .card-container {
+        max-width: 1400px;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+
+    props: {
+        ComicInfo: Object,
+    },
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="ComicInfo.thumb" alt="comic">
+        <h3>{{ ComicInfo.series }}</h3>
+        <p>{{ ComicInfo.price }}</p>
+        <p>{{ ComicInfo.type }}</p>
+    </div>
+</template>
+
+<style lang="scss"  scoped>
+.card {
+    width: calc(15% - 40px);
+    margin: 0 20px;
+
+
+
+
+}
+</style>
